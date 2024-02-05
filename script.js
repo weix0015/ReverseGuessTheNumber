f2c9ad75e248ef68bc8cd42089f45fe050bcfe40
Wei Yang <weix0015@stud.kea.dk>
2024-02-05 14:17:38 +0100

    added binary search algorithm to guess the number

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // disable all buttons except start
 // disable all buttons after click on
 window.addEventListener("load", function() {
-  document.getElementById("start").addEventListener("click", start);
+  document.getElementById("start").addEventListener("click", startGame);
   document.getElementById("correct").addEventListener("click", correctGuess);
   document.getElementById("smaller").addEventListener("click", guessSmall);
   document.getElementById("higher").addEventListener("click", guessHigher);
@@ -14,19 +14,21 @@ window.addEventListener("load", function() {
 // number from 1 - 100
 let min = 1;
 let max = 100;
+let start = min;
+let end = max;
 
 // random guess
-let guess;
+let guess = Math.floor((start + end) / 2);
 
 // check if game won
 let gameWon = false;
 
 // generate random number after click on start button
 // disable start button after clicking on it / hide start button
-function start() {
+function startGame() {
   console.log("Javascript is running");
   document.getElementById("result").innerText = "";
-  guess = Math.floor(Math.random() * (max - min) + min);
+  guess = Math.floor((start + end) / 2);
   displayGuess();
   // disable start buttons after clicking start / hide start button
   document.getElementById("start").style.display = "none";
@@ -36,9 +38,8 @@ function start() {
 // guess smaller
 function guessSmall() {
   if (!gameWon) {
-    // random number lower than previous guess - lowest 1
-    max = guess - 1;
-    guess = Math.floor(Math.random() * (max - min + 1) + min);
+    end = guess;
+    guess = Math.floor((start + end) / 2);
     displayGuess();
   }
 }
@@ -46,9 +47,8 @@ function guessSmall() {
 // guess higher
 function guessHigher() {
   if (!gameWon) {
-    // random number higher than previous guess - max 100
-    min = guess + 1;
-    guess = Math.floor(Math.random() * (max - min) + min);
+    start = guess + 1;
+    guess = Math.floor((start + end) / 2);
     displayGuess();
   }
 }
