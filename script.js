00b87158c3de0c4acc2eb40ad699298f16c76991
Wei Yang <weix0015@stud.kea.dk>
2024-02-07 15:02:35 +0100

    added binary search with while loop, compare and recursive

--- script.js
+++ script.js
@@ -79,3 +79,85 @@ function enableButtons() {
   document.getElementById("higher").style.display = "inline-block";
   document.getElementById("correct").style.display = "inline-block";
 }
+
+// binary search with while loop
+const values = [1,2,3,5,7,8,9,11,12,14,15];
+
+function binarySearch( value, values ) {
+	let start = 0, end = values.length -1, iterations = 0;
+
+  const maxIterations = Math.ceil(Math.log2(values.length));
+
+  while (start <= end && iterations < maxIterations) {
+  let middle = Math.floor((start + end) / 2);
+  console.log(`start=${start}, middle=${middle}, end=${end}`);
+
+  if (value === values[middle]) return middle;
+  value > values[middle] ? start = middle + 1 : end = middle - 1;
+    start = middle + 1;
+  }
+  return -1;
+}
+
+console.log(binarySearch(8, values));
+console.log(binarySearch(10, values));
+
+// binary search with compare function
+function binarySearchFunction(value, values, compare) {
+  let start = 0, end = values.length -1, iterations = 0;
+
+  const maxIterations = Math.ceil(Math.log2(values.length));
+
+  while (start <= end && iterations < maxIterations) {
+    let middle = Math.floor((start + end) / 2);
+
+    const comparisonResult = compare(value, values[middle]);
+
+    if (comparisonResult === 0) return middle;
+    if (comparisonResult === 1) start = middle + 1;
+    else end = middle - 1;
+
+    iterations++;
+  }
+  return -1;
+}
+
+// comparing strings
+function stringCompare(search, check) {
+  return search.localeCompare(check, "da");
+}
+
+// comparing object
+function nameCheck(search, check) {
+  return search.localeCompare(check.name);
+}
+
+// test strings
+const ordliste = ["andedam", "andegård", "bondefanget", 
+"bondegård", "børnearbejde", "gadefejer", "gær", 
+"gødning", "gaardejer", "kalapøjser", "kalundborg", 
+"kørt", "kårde", "ålborg", "aarhus"];
+console.log(binarySearchFunction("gaardejer", ordliste, stringCompare));
+
+// test with objects
+const persons = [{ name: "Draco Malfoy", house: "Slytherin" }, 
+{ name: "Harry Potter", house: "Gryffindor" }, 
+{ name: "Hermione Granger", house: "Gryffindor" }, 
+{ name: "Neville Longbottom", house: "Gryffindor" }, 
+{ name: "Ron Weasley", house: "Gryffindor" }];
+console.log(binarySearchFunction("Ron Weasley", persons, nameCheck));
+
+
+// binary search recursive
+function binarySearchRecursive(value, values, start, end) {
+  if (start > end) return -1;
+
+  let middle = Math.floor((start + end) / 2);
+
+  return value === values[middle] ? middle :
+         value > values[middle] ? binarySearchRecursive(value, values, middle + 1, end) :
+         binarySearchRecursive(value, values, start, middle - 1);
+}
+
+console.log(binarySearchRecursive(8, values, 0, values.length -1));
+console.log(binarySearchRecursive(10, values, 0, values.length -1));
